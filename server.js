6e5827f629c6cc9bd219e25a79a9a5b0a8d6916f
gpatruno <56818714+gpatruno@users.noreply.github.com>
2019-12-07 14:29:38 +0100

    Model db + get db work

--- server.js
+++ server.js
@@ -9,56 +9,37 @@ const http = require('http').createServer(server);
 
 
 require('./routes/armors.js')(server);
-
-server.use(express.static(path.join(__dirname, 'routes'))); //Récupère l'index.html dans le dossier maître "client"
+//server.use(express.static(path.join(__dirname, 'routes'))); //Récupère l'index.html dans le dossier maître "client"
 
 // require('./routes/armors.js')(server,fs);
 // require('./routes/armors.js')(server,fs);
 // require('./routes/armors.js')(server,fs);
 
-// server.get('/db', function (req, res) {
-//     console.log('Get Users');
-//     res.setHeader('Content-Type', 'text/json');
-//     res.status(200);
-//     res.send('{"success": "true","msg": "Users", "result":[]}');
-// });
-
-// //Configure routes
-// server.get('/db', function (req, res) {
-//     if (req) {
-
-//     }
-//     if (res) {
-
-//     }
-//     fs.readFile('/', function (err, data) {
-//         console.log('Get DB');
-//         if (data) {
-//             res.setHeader('Content-Type', 'text/json');
-//             res.status(200);
-//             res.send('{"success": "true","msg": "File loaded", "result":' + data + '}');
-//         }
-//         if (err) {
-//             res.setHeader('Content-Type', 'text/json');
-//             res.status(500);
-//             res.send('{"success": "false","msg": "Failure read = ' + path + '", "result":' + data + '}');
-//         }
-//     });
-// });
-
-// function getData(path, res) {
-
-// }
+//Configure routes
+server.get('/db', function (req, res) {
+    if (req) {
+        console.log('test1');
+    }
+    if (res) {
+        console.log('test2');
+    }
+    fs.readFile('./db.json', function (err, data) {
+        console.log('Get DB');
+        if (data) {
+            res.setHeader('Content-Type', 'text/json');
+            res.status(200);
+            res.send('{"success": "true","msg": "File loaded", "result":' + data + '}');
+        }
+        if (err) {
+            res.setHeader('Content-Type', 'text/json');
+            res.status(500);
+            res.send('{"success": "false","msg": "Failure read file", "result": null}');
+        }
+    });
+});
 
 
-// server.get('/db', function (req, res) {
-//     console.log('Get Users');
-//     res.setHeader('Content-Type', 'text/json');
-//     res.status(200);
-//     res.send('{"success": "true","msg": "Users", "result":[]}');
-// });
-
-// //Lunch server
-// server.listen(3000, function () {
-//     console.log('server started on port 3000');
-// });
+//Lunch server
+server.listen(3000, function () {
+    console.log('server started on port 3000');
+});
