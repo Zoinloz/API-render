2d115f6787d7b43f8e8c5d3a7c5429937e4a8c88
Blanc Kévin <56817478+Zoinloz@users.noreply.github.com>
2020-01-16 11:25:02 +0100

    Update armor.model.js

--- model/armor.model.js
+++ model/armor.model.js
@@ -6,7 +6,11 @@ const armorSchema = new mongoose.Schema({
  id: { type: String, required: true, unique: true, trim: true },
  name: { type: String, required: true, unique: true, trim: true },
  cost: { type: Number, required: true },  
-helmet: { type: mongoose.Types.ObjectId, ref: "Helmet" },
+ helmet: { type: mongoose.Types.ObjectId, ref: "Helmet" },
+ chest: { type: mongoose.Types.ObjectId, ref: "Chest" },
+ cloak: { type: mongoose.Types.ObjectId, ref: "Cloak" },
+ legs: { type: mongoose.Types.ObjectId, ref: "Legs" },
+ arm: { type: mongoose.Types.ObjectId, ref: "Arm" },
 });
 
 mongoose.model('Armor', armorSchema);
