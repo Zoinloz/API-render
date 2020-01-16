60eaf9a295a3a71bb35dcba5c0ce19f1214aca9c
gpatruno <56818714+gpatruno@users.noreply.github.com>
2020-01-16 11:32:07 +0100

    Fix Kéma Armor.model  (Fix is not sure)

--- model/armor.model.js
+++ model/armor.model.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose');
 const armorSchema = new mongoose.Schema({
  id: { type: String, required: true, unique: true, trim: true },
  name: { type: String, required: true, unique: true, trim: true },
- cost: { type: Number, required: true },  
- helmet: { type: mongoose.Types.ObjectId, ref: "Helmet" },
- chest: { type: mongoose.Types.ObjectId, ref: "Chest" },
- cloak: { type: mongoose.Types.ObjectId, ref: "Cloak" },
- legs: { type: mongoose.Types.ObjectId, ref: "Legs" },
- arm: { type: mongoose.Types.ObjectId, ref: "Arm" },
+ composition: {
+   helmet: { type: mongoose.Types.ObjectId, ref: "Helmet" },
+   chest: { type: mongoose.Types.ObjectId, ref: "Chest" },
+   cloak: { type: mongoose.Types.ObjectId, ref: "Cloak" },
+   legs: { type: mongoose.Types.ObjectId, ref: "Legs" },
+   arm: { type: mongoose.Types.ObjectId, ref: "Arm" }
+ }
+
 });
 
 mongoose.model('Armor', armorSchema);
