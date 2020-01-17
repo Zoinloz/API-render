63c5808fb82f1bac4adf1c307d15d5a2a099a360
Zoinloz <blanckevin19@gmail.com>
2020-01-17 09:43:42 +0100

    ...

--- routes/racine.js
+++ routes/racine.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render("layouts/racine/racine", {
-        viewTitle: "Insert Project"
+        viewTitle: "Read.ME"
     });
 });
 
