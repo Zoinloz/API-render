29dabf4c6cc381dbe41b9c34e4f415438264bc20
gpatruno <56818714+gpatruno@users.noreply.github.com>
2020-01-17 23:26:09 +0100

    Prepare Chest View

--- routes/chests.js
+++ routes/chests.js
@@ -30,58 +30,6 @@ router.get('/list', (req, res) => {
             });
         });
     }).end();
-const mongoose = require('mongoose');
-const Chest = mongoose.model('Chest');//Get the Chest model
-
-//Get the list of all Chest with url: http://localhost:3000/chests   Method : GET
-router.get('/', (req, res) => {
-    Chest.find((err, docs) => {
-        if (!err) {
-            if (docs.length != 0) {
-                res.status(200);
-                res.send({ 'msg': 'the Chests list !', 'success': 'true', 'result': docs });
-               
-            } else {
-                res.status(200);
-                res.send({ 'msg': 'No data !', 'success': 'true', 'result': docs });
-            }
-        } else {
-            res.status(500);
-            res.send({'msg': 'Something goes wrong !', 'success': 'false', 'result': err });
-        }
-    });
-});
-
-//Get one of Chest with the name url: http://localhost:3000/chests/<name>   Method : GET
-router.get('/:name', (req, res) => {
-    Chest.findOne({name: req.params.name}, (err, doc) => {
-        if (!err && doc!= null) {
-            res.status(200).send({ 'msg': 'Chest found !', 'success': 'true', 'result': doc });
-        }else{
-            res.status(500).send({ 'msg': 'Chest not found', 'success': 'false', 'result': err });
-        }
-    });
-});
-
-
-//Add a chest with url : http://localhost:3000/chests/add    Method : POST
-//need only 2 parameters in body request : name & value
-router.post('/add', (req, res) => {
-    let chest = new Chest();
-    chest.type = "Chest";
-    chest.name = req.body.name;
-    chest.value = req.body.value;
-
-    chest.save((err, doc) => {
-        if (!err) {
-            res.status(201).send({ 'msg': 'Chest Added !', 'success': 'true', 'result': doc });
-        } else {
-            res.status(500).send({ 'msg': 'Smothing goes wrong !', 'success': 'false', 'result': err });
-        }
-    });
-});
-
-});
 
 router.get('/add', (req, res) => {
     res.render("layouts/chest/addOrEdit", {
