1a8c4af6bc6f3171fa144a4c8de8bc5274bcacee
FairyPuck <julian.hamboyan@gmail.com>
2020-01-19 19:45:25 +0100

    Cloak & Arm are OK !

--- routes/arms.js
+++ routes/arms.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const http = require('http');
 
-//Show table result of the Chest list with result request
+//Show table result of the arm list with result request
 //Call API Armor
 router.get('/list', (req, res) => {
     const optionsGet = {
@@ -30,9 +30,17 @@ router.get('/list', (req, res) => {
     }).end();
 });
 
-//call Api to Add a Chest by the View
 router.post('/', (req, res) => {
-    console.log(req.body);
+    console.log('ROUTERRRRR',req.body);
+    if (req.body._id == '') {
+        insertRecord(req, res);
+    } else {
+        updateRecord(req, res);
+    }
+});
+
+function insertRecord(req, res) {
+    console.log('INSERTTTT',req.body);
     const optionsPost = {
         host: 'localhost',
         port: 3000,
@@ -55,36 +63,60 @@ router.post('/', (req, res) => {
             });
         });
     }).end();
-});
-router.post('', (req, res) => {
-    
-});
+}
 
-//Render the view to add chest
+function updateRecord(req, res) {
+    console.log('UPDATEEEE',req.body);
+    const optionsPost = {
+        host: 'localhost',
+        port: 3000,
+        path: '/arms/'+req.body.Id+'&'+req.body.Name+'&'+req.body.Value,
+        method: 'PUT'
+    };
+    console.log('PATHHH',optionsPost)
+    http.request(optionsPost, function (result) {
+        console.log('STATUS: ' + result.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(result.headers));
+        result.setEncoding('utf8');
+        result.on('data', function (chunk) {
+            console.log(JSON.parse(chunk));
+            res.render("layouts/arm/info", {
+                viewTitle: 'Result of Updated arm',
+                statut: result.statusCode,
+                success: JSON.parse(chunk).success,
+                message: JSON.parse(chunk).msg,
+                arm: JSON.parse(chunk).result,
+            });
+        });
+    }).end();
+}
+
+//Render the view to add arm
 router.get('/add', (req, res) => {
     res.render("layouts/arm/addOrEdit", {
-        viewTitle: "Insert Arm",
+        viewTitle: "Insert arm",
         statut: 'NaN',
         success: 'NaN',
-        message: 'Create your Arm',
+        message: 'Create your arm',
     });
 });
 
 //Render view to Update a arm, param is ObjectId and Name and Value
 router.get('/:id', (req, res) => {
+    console.log('WTF',req.params);
     const optionsGetById = {
         host: 'localhost',
         port: 3000,
-        path: '/arms/'+req.params.id+'&'+req.body.Name+'&'+req.body.Value,
-        method: 'PUT'
+        path: '/arms/'+req.params.id,
+        method: 'GET'
     };
     console.log('PATH',optionsGetById);
     //Parameters of the request
     http.request(optionsGetById, function (result) {
         result.setEncoding('utf8');
         result.on('data', function (chunk) {
-            res.render("layouts/arm/list", {
-                viewTitle: 'Update Arm',
+            res.render("layouts/arm/addOrEdit", {
+                viewTitle: 'Update arm',
                 statut: result.statusCode,
                 success: JSON.parse(chunk).success,
                 message: JSON.parse(chunk).msg,
@@ -94,7 +126,7 @@ router.get('/:id', (req, res) => {
     }).end();
 });
 
-//Delete Arm by ObjectID
+//Delete arm by ObjectID
 router.get('/delete/:id', (req, res) => {
     const optionsGetById = {
         host: 'localhost',
--- routes/cloaks.js
+++ routes/cloaks.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const http = require('http');
 
-//Show table result of the Chest list with result request
+//Show table result of the Cloak list with result request
 //Call API Armor
 router.get('/list', (req, res) => {
     const optionsGet = {
@@ -30,9 +30,17 @@ router.get('/list', (req, res) => {
     }).end();
 });
 
-//call Api to Add a Chest by the View
 router.post('/', (req, res) => {
-    console.log(req.body);
+    console.log('ROUTERRRRR',req.body);
+    if (req.body._id == '') {
+        insertRecord(req, res);
+    } else {
+        updateRecord(req, res);
+    }
+});
+
+function insertRecord(req, res) {
+    console.log('INSERTTTT',req.body);
     const optionsPost = {
         host: 'localhost',
         port: 3000,
@@ -55,12 +63,35 @@ router.post('/', (req, res) => {
             });
         });
     }).end();
-});
-router.post('', (req, res) => {
-    
-});
+}
 
-//Render the view to add chest
+function updateRecord(req, res) {
+    console.log('UPDATEEEE',req.body);
+    const optionsPost = {
+        host: 'localhost',
+        port: 3000,
+        path: '/cloaks/'+req.body.Id+'&'+req.body.Name+'&'+req.body.Value,
+        method: 'PUT'
+    };
+    console.log('PATHHH',optionsPost)
+    http.request(optionsPost, function (result) {
+        console.log('STATUS: ' + result.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(result.headers));
+        result.setEncoding('utf8');
+        result.on('data', function (chunk) {
+            console.log(JSON.parse(chunk));
+            res.render("layouts/cloak/info", {
+                viewTitle: 'Result of Updated cloak',
+                statut: result.statusCode,
+                success: JSON.parse(chunk).success,
+                message: JSON.parse(chunk).msg,
+                cloak: JSON.parse(chunk).result,
+            });
+        });
+    }).end();
+}
+
+//Render the view to add cloak
 router.get('/add', (req, res) => {
     res.render("layouts/cloak/addOrEdit", {
         viewTitle: "Insert Cloak",
@@ -70,21 +101,22 @@ router.get('/add', (req, res) => {
     });
 });
 
-//Render view to Update a chest, param is ObjectId and Name and Value
+//Render view to Update a cloak, param is ObjectId and Name and Value
 router.get('/:id', (req, res) => {
+    console.log('WTF',req.params);
     const optionsGetById = {
         host: 'localhost',
         port: 3000,
-        path: '/cloaks/'+req.params.id+'&'+req.body.Name+'&'+req.body.Value,
-        method: 'PUT'
+        path: '/cloaks/'+req.params.id,
+        method: 'GET'
     };
     console.log('PATH',optionsGetById);
     //Parameters of the request
     http.request(optionsGetById, function (result) {
         result.setEncoding('utf8');
         result.on('data', function (chunk) {
-            res.render("layouts/cloak/list", {
-                viewTitle: 'Update Cloak',
+            res.render("layouts/cloak/addOrEdit", {
+                viewTitle: 'Update cloak',
                 statut: result.statusCode,
                 success: JSON.parse(chunk).success,
                 message: JSON.parse(chunk).msg,
@@ -94,7 +126,7 @@ router.get('/:id', (req, res) => {
     }).end();
 });
 
-//Delete Chest by ObjectID
+//Delete cloak by ObjectID
 router.get('/delete/:id', (req, res) => {
     const optionsGetById = {
         host: 'localhost',
